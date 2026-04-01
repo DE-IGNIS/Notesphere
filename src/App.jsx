c3e75199455cd3df6ccada8a9ef11e4bbf7e0409
DE-IGNIS <chauhansudarsh@gmail.com>
2026-04-01 23:24:31 +0530

    Update : removing theme context

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
 import { BrowserRouter, Routes, Route, Form } from "react-router-dom";
-import Dashboard, { ThemeContext } from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard";
 import NotePage from "./pages/NotePage";
-import { useContext } from "react";
 import "./App.css";
 
 function App() {
-  const { theme } = useContext(ThemeContext);
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <div className={`app-shell ${theme}`}>
-          <header className="app-header">
-            <h1 className="app-title">NoteSphere</h1>
-          </header>
+    <BrowserRouter>
+      <div className={`app-shell`}>
+        <header className="app-header">
+          <h1 className="app-title">NoteSphere</h1>
+        </header>
 
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/note/:id" element={<NotePage />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </ThemeProvider>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/note/:id" element={<NotePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
